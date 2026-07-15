c563dc398d68f37462384b661a4f7706b590cc06
Hemanth70195 <133030591+Hemanth70195@users.noreply.github.com>
2026-07-15 14:47:12 +0530

    Time: 952 ms (5.00%) | Memory: 411.3 MB (8.42%) - LeetSync

--- Sorting/215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ Sorting/215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,15 +1,69 @@
+// class Solution {
+// public:
+//     int findKthLargest(vector<int>& nums, int k) {
+//         priority_queue<int,vector<int>,greater<int>> minheap;
+//         for(int i :nums)
+//         {
+//             minheap.push(i);
+//             if(minheap.size() > k)
+//             {
+//                 minheap.pop();
+//             }
+//         }
+//         return minheap.top();
+//     }
+// };
+
 class Solution {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int,vector<int>,greater<int>> minheap;
-        for(int i :nums)
+
+    void merge(vector<int> &nums,int st,int mid,int end)
+    {   vector<int> temp;
+        int i=st;
+        int j=mid+1;
+        while(i<=mid && j<=end)
         {
-            minheap.push(i);
-            if(minheap.size() > k)
+            if(nums[i] < nums[j])
             {
-                minheap.pop();
+                temp.push_back(nums[i]);
+                i++;
+            }
+            else
+            {
+                temp.push_back(nums[j]);
+                j++;
             }
         }
-        return minheap.top();
+        while(i<=mid)
+        {
+            temp.push_back(nums[i]);
+                i++;
+        }
+        while(j<=end)
+        {
+            temp.push_back(nums[j]);
+                j++;
+        }
+        for(int i=0;i<temp.size();i++)
+        {
+            nums[i+st]= temp[i];
+        }
+    }
+
+
+    void mergesort(vector<int> &nums,int st,int end)
+    {
+        if(st<end)
+        {
+            int mid = st + (end-st)/2;
+            mergesort(nums,st,mid);
+            mergesort(nums,mid+1,end);
+            merge(nums,st,mid,end);
+        }
+    }
+
+    int findKthLargest(vector<int>& nums, int k) {
+        mergesort(nums,0,nums.size()-1);
+        return nums[nums.size()-k];
     }
 };
