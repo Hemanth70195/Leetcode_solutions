812300b1d47f7b41e2d9ca4747eba06072fc006f
Hemanth70195 <133030591+Hemanth70195@users.noreply.github.com>
2026-07-23 14:08:51 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (65.53%) - LeetSync

---
+++ Linked_List/71-simplify-path/simplify-path.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stringstream ss(path);
+        string token;
+        vector<string> st;
+
+        while(getline(ss,token,'/'))
+        {
+            if(token == "" || token==".")
+                continue;
+
+            else if(token == "..")
+            {
+                if(!st.empty())
+                    st.pop_back();
+            }
+            else
+                st.push_back(token);
+        }
+        string res="";
+        for(const string& dir : st)
+        {
+            res+="/" + dir;
+        }
+       return res.empty() ? "/" : res; 
+    }
+};
