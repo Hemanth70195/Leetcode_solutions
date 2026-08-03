818b71c573944c9293de01738f6f358d4a0588b2
Hemanth70195 <133030591+Hemanth70195@users.noreply.github.com>
2026-08-03 10:18:16 +0530

    Time: 0 ms (100.00%) | Memory: 24 MB (77.76%) - LeetSync

---
+++ Binary_Trees/605-can-place-flowers/can-place-flowers.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& a, int n) {
+        if(n==0)    return true;
+        int size=a.size();
+        if(size==1)
+        {
+            if(a[0] == 0 && n==1)   return true;
+        }
+
+        for(int i=0;i<size;i++)
+        {
+            if(i==0)
+            {
+                if(a[i] == 0 && a[i+1] == 0)    
+                {
+                    n--;
+                     if(n==0)return true;
+                    a[i]=1;
+                }
+            }
+            else if(i==size-1)
+            {
+                if(a[size-1] == 0 && a[size-2] == 0)
+                {
+                    n--;
+                    if(n==0)    return true;
+                    a[i]=1;
+                }
+
+            }
+            else
+            {
+                if(a[i] ==0 && a[i-1] == 0 && a[i+1] == 0)
+                {
+                    n--;
+                    if(n==0)    return true;
+                    a[i]=1;
+                }
+            }
+        }
+        return false;
+    }
+};
